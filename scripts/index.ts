bbabe038e165843a28b823d99e51f184043f44f0
zhangchao <529743595@qq.com>
2022-06-05 23:07:43 +0800

    feat: add generate

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,47 +1,48 @@
 import * as allIconDefs from '@ant-design/icons-svg';
 import * as path from 'path';
 import * as fs from 'fs';
+import { promisify } from 'util';
 
+const writeFile = promisify(fs.writeFile);
 console.log(allIconDefs.AlignCenterOutlined);
-// Object.keys(allIconDefs).map(async (icon) => {
-//     await fs.writeFileSync(
-//         path.resolve(__dirname, `../src/icons/${icon}.tsx`),
-//         `
-// // GENERATE BY ./scripts/generate.ts
-// // DON NOT EDIT IT MANUALLY
-//
-// import { FunctionalComponent } from 'vue';
-// import <%= svgIdentifier %>Svg from '@ant-design/icons-svg/lib/asn/<%= svgIdentifier %>';
-// import AntdIcon, { AntdIconProps } from '../components/AntdIcon';
-//
-// export interface <%= svgIdentifier %>IconType extends FunctionalComponent<AntdIconProps> {
-//   displayName: string;
-// }
-//
-// const <%= svgIdentifier %>: <%= svgIdentifier %>IconType = (props, context) => {
-//   const p = { ...props, ...context.attrs };
-//   return <AntdIcon {...p} icon={<%= svgIdentifier %>Svg}></AntdIcon>;
-// };
-//
-// <%= svgIdentifier %>.displayName = '<%= svgIdentifier %>';
-// <%= svgIdentifier %>.inheritAttrs = false;
-// export default <%= svgIdentifier %>;
-// `.trim()
-//     )
-// })
+
 const generateIcons = async () => {
     const iconsDir = path.join(__dirname, '../src/icons');
-    console.log(iconsDir, 111);
     try {
         await fs.accessSync(iconsDir);
     } catch (err) {
         await fs.mkdirSync(iconsDir);
     }
-    Object.keys(allIconDefs).map(item => {
-        console.log(item);
-    });
+    // generate icon file
+    Promise.all(
+        Object.keys(allIconDefs).map(svgIdentifier => {
+            writeFile(
+                path.resolve(__dirname, `../src/icons/${svgIdentifier}.tsx`),
+                `
+// GENERATE BY ./scripts/generate.ts
+// DON NOT EDIT IT MANUALLY
+export default '${svgIdentifier}'
+                `.trim()
+            );
+        })
+    );
 
-    console.log(1111);
+    // generate icon index
+    const entryText = Object.keys(allIconDefs)
+        .sort()
+        .map(svgIdentifier => {
+            return `export { default as ${svgIdentifier} } from './${svgIdentifier}';`;
+        })
+        .join('\n');
+
+    fs.appendFileSync(
+        path.resolve(__dirname, '../src/icons/index.tsx'),
+        `
+// GENERATE BY ./scripts/generate.ts
+// 不要手动修改！！！
+${entryText}
+        `.trim()
+    );
 };
 
-// generateIcons()
+generateIcons();
