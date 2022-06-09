19828ad80914ef8a127eefd62b2907c4b007cd07
zhangchao <529743595@qq.com>
2022-06-09 17:57:23 +0800

    feat: add file dir

--- scripts/index.ts
+++ scripts/index.ts
@@ -2,57 +2,76 @@ import * as allIconDefs from '@ant-design/icons-svg';
 import * as path from 'path';
 import * as fs from 'fs';
 import { promisify } from 'util';
+import { createDir } from './utils';
 
 const writeFile = promisify(fs.writeFile);
-
-const generateIcons = async () => {
-    const iconsDir = path.join(__dirname, '../src/icons');
-    try {
-        await fs.accessSync(iconsDir);
-    } catch (err) {
-        await fs.mkdirSync(iconsDir);
-    }
-    // generate icon file
-    Promise.all(
-        Object.keys(allIconDefs).map(svgIdentifier => {
-            writeFile(
-                path.resolve(__dirname, `../src/icons/${svgIdentifier}.tsx`),
-                `
+const codeRemark = `
 // GENERATE BY ./scripts/generate.ts
 // 不要手动修改！！！
+`.trim();
+
+const generateIcons = async () => {
+    await createDir('../src/icons');
+    // generate icon file
+    Promise.all(
+        Object.keys(allIconDefs).map(async svgIdentifier => {
+            await createDir(`../src/icons/${allIconDefs[svgIdentifier].theme}`);
+            const fileName = `Icon${svgIdentifier}`;
+            await writeFile(
+                path.resolve(__dirname, `../src/icons/${allIconDefs[svgIdentifier].theme}/${fileName}.tsx`),
+                `
+${codeRemark}
 
 import { FunctionalComponent } from 'vue';
-import Icon, { IconProps } from '../components/Icon';
-import ${svgIdentifier}Svg from '@ant-design/icons-svg/lib/asn/${svgIdentifier}';
+import Icon, { IconProps } from '../../components/Icon';
+import ${svgIdentifier}Svg from '@ant-design/icons-svg/es/asn/${svgIdentifier}';
 
 export interface ${svgIdentifier}IconType extends FunctionalComponent<IconProps> {
     displayName: string;
 }
 
-const ${svgIdentifier}: ${svgIdentifier}IconType = (props, context) => <Icon name='${svgIdentifier}' {...{ ...props, ...context.attrs }} icon={${svgIdentifier}Svg} />;
+const ${fileName}: ${svgIdentifier}IconType = (props, context) => (
+    <Icon name='${svgIdentifier}' {...{ ...props, ...context.attrs }} icon={${svgIdentifier}Svg} />
+);
 
-${svgIdentifier}.displayName = '${svgIdentifier}';
+${fileName}.displayName = '${fileName}';
 
-export default ${svgIdentifier};
+export default ${fileName};
                 `.trim()
             );
         })
     );
 
     // generate icon index
-    const entryText = Object.keys(allIconDefs)
-        .sort()
-        .map(svgIdentifier => {
-            return `export { default as ${svgIdentifier} } from './${svgIdentifier}';`;
-        })
-        .join('\n');
+    const themes = Array.from(new Set(Object.values(allIconDefs).map(v => v.theme))).sort();
+    themes.map(theme => {
+        const entryText = Object.keys(allIconDefs)
+            .sort()
+            .filter(svgIdentifier => allIconDefs[svgIdentifier].theme === theme)
+            .map(svgIdentifier => {
+                return `export { default as Icon${svgIdentifier} } from './Icon${svgIdentifier}';`;
+            })
+            .join('\n');
+        fs.appendFileSync(
+            path.resolve(__dirname, `../src/icons/${theme}/index.tsx`),
+            `
+${codeRemark}
 
+${entryText}
+        `.trim()
+        );
+        return theme;
+    });
     fs.appendFileSync(
         path.resolve(__dirname, '../src/icons/index.tsx'),
         `
-// GENERATE BY ./scripts/generate.ts
-// 不要手动修改！！！
-${entryText}
+${codeRemark}
+
+${themes
+    .map(theme => {
+        return `export * from './${theme}/index';`;
+    })
+    .join('\n')}
         `.trim()
     );
 };
