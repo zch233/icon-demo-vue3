59f447414539937a7ebebf756fb3a595e2e1c4bc
zhangchao <529743595@qq.com>
2022-06-09 23:49:48 +0800

    feat: support two tone

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,6 +1,7 @@
 import { FunctionalComponent, HTMLAttributes } from 'vue';
 import { AbstractNode, IconDefinition } from '@ant-design/icons-svg/es/types';
-import { generateSvgNode, useInsertStyles } from "../utils";
+import { generateSvgNode, useInsertStyles } from '../utils';
+import { generate as generateColor } from '@ant-design/colors';
 
 export interface IconProps extends HTMLAttributes {
     spin?: boolean;
@@ -8,11 +9,13 @@ export interface IconProps extends HTMLAttributes {
     size?: number;
     icon?: IconDefinition;
     name?: string;
+    primaryColor?: string; // only for two-tone
+    secondaryColor?: string; // only for two-tone
 }
 
 const Icon: FunctionalComponent<IconProps> = (props, context) => {
     const { attrs, slots } = context;
-    const { spin, rotate, size, icon, name, ...restProps } = { ...props, ...attrs };
+    const { spin, rotate, size, icon, name, primaryColor, secondaryColor, ...restProps } = { ...props, ...attrs };
     const children = slots.default && slots.default();
     const classResult = {
         gupoIcon: true,
@@ -34,11 +37,20 @@ const Icon: FunctionalComponent<IconProps> = (props, context) => {
             : undefined),
     };
     useInsertStyles();
-    return icon ? (
+
+    let target = icon;
+    if (target && typeof target.icon === 'function') {
+        const color = primaryColor || '#1890ff';
+        target = {
+            ...target,
+            icon: target.icon(color, secondaryColor ? generateColor(secondaryColor)[0] : generateColor(color)[0]),
+        };
+    }
+    return target ? (
         <span role='img' aria-label={name} {...restProps} class={classResult} style={styleResult}>
-            {generateSvgNode(icon.icon as AbstractNode, `gupoIcon-svg-${icon.name}`, {
+            {generateSvgNode(target.icon as AbstractNode, `gupoIcon-svg-${target.name}`, {
                 class: 'gupoIcon-icon',
-                'data-icon': icon.name,
+                'data-icon': target.name,
             })}
         </span>
     ) : (
