254660eed28ab21804a281b17a0a64da65de9c33
zhangchao <529743595@qq.com>
2022-06-19 18:31:33 +0800

    feat: update versions

--- src/icons/colorful/index.tsx
+++ src/icons/colorful/index.tsx
@@ -1,4 +1,4 @@
 // GENERATE BY ./scripts/generate.ts
 // 不要手动修改！！！
 
-export { default as IconImageColor } from './IconImageColor';
+export { default as IconImageColorful } from './IconImageColorful';
